c63e472ca37de59fa20ec6e34953725189ba2c50
Raihan Zulfi <114371959+raizulfi@users.noreply.github.com>
2022-12-03 06:42:19 +0700

    Update main_b_time_iterative.c

--- main_b_time_iterative.c
+++ main_b_time_iterative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include "mylib/mylib.h"
 int main(void){
-    const int N = 10;
+    const int N = 40;
     float StartTime = (float)clock()/CLOCKS_PER_SEC;
     for (unsigned int i = 0; i < N; i++) {
         fibonacciIterative(i);
